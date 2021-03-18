6d67a30ba5ed5c1ecc2a205371d62dce234b8b72
Ismaeel Shujat <ismaeelshujat@gmail.com>
2021-03-18 17:19:11 +0500

    Changes boundry logic from 4 boundry boxes with 2d box colliders to 2d edge collider.

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,6 +5,7 @@ public class BallController : MonoBehaviour
     private Rigidbody2D rigidBody;
     private float speed;
     private Vector2 ballSpawnPosition;
+    private SpriteRenderer spriteRenderer;
 
     public delegate void BallDroped();
     public event BallDroped OnBallDropped;
@@ -13,22 +14,27 @@ public class BallController : MonoBehaviour
     {
         speed = 5;
         rigidBody = GetComponent<Rigidbody2D>();
-        Reset();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        ResetBall();
     }
 
-    private void Reset()
+    private void ResetBall()
     {
         ballSpawnPosition = new Vector2(0,-3);
         transform.position = ballSpawnPosition;
         rigidBody.velocity = Random.insideUnitCircle.normalized * speed;
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D other) 
     {
-        if(other.gameObject.name.Equals("Bottom"))
+        //Edge collider only exists on the boundry of the scene.
+        if(other.gameObject.GetComponent<EdgeCollider2D>())
         {
-            OnBallDropped?.Invoke();
-            Reset();
+            if( transform.position.y - spriteRenderer.size.y <= other.collider.bounds.min.y + spriteRenderer.size.y)
+            {
+                OnBallDropped?.Invoke();
+                ResetBall();
+            }
         }
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        CreateBoundryEdges();
+    }
+	private void CreateBoundryEdges()
+	{
+		List<Vector2> newVertices = new List<Vector2>();
+		EdgeCollider2D edgeCollider = this.GetComponent<EdgeCollider2D>();
+		
+		//Get the boundaries of the camera
+		float camDistance = Vector3.Distance(this.transform.position, Camera.main.transform.position);
+		Vector2 bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, camDistance));
+		Vector2 topRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, camDistance));
+		
+		//Set these to the new vertices for the Edge Collider
+		newVertices.Add (new Vector2(bottomLeft.x, bottomLeft.y)); // Sets the start point
+		newVertices.Add (new Vector2(bottomLeft.x, topRight.y)); // Creates the Top Side
+		newVertices.Add (new Vector2(topRight.x, topRight.y)); // Creates the Right Side
+		newVertices.Add (new Vector2(topRight.x, bottomLeft.y)); // Creates the Bottom Side
+		newVertices.Add (new Vector2(bottomLeft.x, bottomLeft.y)); //Creates the Left Side
+		
+		//Update the edge collider
+		edgeCollider.points = newVertices.ToArray();
+	}
+
+
+
+}
